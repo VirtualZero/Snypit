75fe78fa85396ea5797dc19226bf5959122851ef
Birtchum <birtchum@virtualzero.net>
2019-03-21 19:31:35 -0400

    Added assets/minification

--- snypit/__init__.py
+++ snypit/__init__.py
@@ -4,12 +4,14 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
+from flask_assets import Bundle, Environment
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ['APP_SECRET_KEY']
 bcrypt = Bcrypt(app)
 csrf = CSRFProtect(app)
+assets = Environment(app)
 
 # SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQL_DATABASE_URI']
@@ -17,6 +19,21 @@ app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# Assets
+js = Bundle(
+    'js/materialize.min.js', 
+    output='assets/snypit.js', 
+    filters='jsmin'
+)
+css = Bundle(
+    'css/style.css', 
+    output='assets/snypit.css', 
+    filters='cssmin'
+)
+assets.register('snypit_js', js)
+assets.register('snypit_css', css)
+
 
 # Models
 from snypit.models.user_models import (
